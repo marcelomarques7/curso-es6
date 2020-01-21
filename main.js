6eac7616dac3ef0f8ef0b2dcf637520945924900
Marcelo Marques <egs.marcelo@gmail.com>
2020-01-21 17:54:14 -0200

    Aulas de classes na metada

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-alert('Teste');
-
-class Teste {
-    metodo(){
-
+class List {
+    constructor(){
+        this.data = [];
     }
 
-    outro(){
-        
+    add(data){
+        this.data.push(data);
+        console.log(this.data);
     }
 }
+
+class TodoList extends List{
+    constructor(){
+        super();
+        
+        this.usuario = 'Marcelo';
+    }
+
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+}
+
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function() {
+    MinhaLista.add('Novo todo');
+}
+
+MinhaLista.mostraUsuario();
